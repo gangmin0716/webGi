9483f3f61e2c52dc3d35fb24601c68c07457adfd
jangkangmin <jkm9780533@gmail.com>
2025-05-30 09:44:38 +0900

    자바스크립트

---
+++ js/web21(js).js
@@ -0,0 +1,15 @@
+// let heading = document.getElementById('heading');
+//         heading.onclick = function() {
+//             heading.style.color = 'green';
+//         }
+
+// alert("홈페이지에 오신 것을 환영합니다")
+// confirm("저장 하시겠습니까?")
+// prompt("이름을 입력하세요")
+
+// document.writeln("<h1>안녕하세요</h1>");
+
+// console.log("안녕하세요");
+
+let name = prompt("이름을 입력해주세요");
+document.writeln(`${name}님 안녕하세요`)
