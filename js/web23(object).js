60e6f449f55b79ffa2784705230ea1f72b8ac8b2
jangkangmin <jkm9780533@gmail.com>
2025-06-20 09:40:13 +0900

    팝업창

--- js/web23(object).js
+++ js/web23(object).js
@@ -1,6 +1,24 @@
-let start = new Date('2025-05-01'); 
-let now = new Date();
+// let start = new Date('2025-05-01'); 
+// let now = new Date();
 
-let pass_day = now.getTime() - start.getTime();
-pass_day = Math.round(pass_day /1000/60/60/24);
-document.getElementById('day').innerText = pass_day
+// let pass_day = now.getTime() - start.getTime();
+// pass_day = Math.round(pass_day /1000/60/60/24);
+// document.getElementById('day').innerText = pass_day
+
+
+
+// 당첨자 뽑기
+
+// let people = parseInt(prompt("전체 응모자수: "));
+// document.getElementById('people').innerText = people;
+// let randam = Math.floor((Math.random() * people) +1);
+// document.getElementById('randam').innerText = randam;
+
+
+// 팝업창
+const b_left = screenX;
+const b_top = screenY;
+
+const p_left = b_left + 100;
+const p_top = b_top + 100;
+open('popup.html', '공지', `width=500, height=400, left = ${p_left}, top=${p_top}`)
